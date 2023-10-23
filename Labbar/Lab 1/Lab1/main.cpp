bdfe0715faf452e696e18087bf7dd3becce32563
nomliF <112778762+nomliF@users.noreply.github.com>
2023-10-23 15:25:49 +0200

    .
    .

--- Labbar/Lab 1/Lab1/main.cpp
+++ Labbar/Lab 1/Lab1/main.cpp
@@ -5,15 +5,25 @@
 using namespace std;
 using namespace this_thread;
 using namespace chrono;
-
-int main()
-{
+void raknaTillTre(){
     sleep_for(milliseconds(500));
     cout << "Ett" << endl;
     sleep_for(milliseconds(500));
     cout << "Tva" <<endl;
-    sleep_for(milliseconds(10000));
+    sleep_for(milliseconds(1000));
     cout << "Tre "<<endl;
     sleep_for(milliseconds(500));
+}
+
+void raknaTillFyra(){
+    raknaTillTre();
+}
+int main()
+{
+    raknaTillFyra();
+    cout << "Rakna forsta gangen: "<<endl;
+    raknaTillTre();
+    cout <<"Rakna andra gangen till"<<endl;
+    raknaTillTre();
     return 0;
 }
