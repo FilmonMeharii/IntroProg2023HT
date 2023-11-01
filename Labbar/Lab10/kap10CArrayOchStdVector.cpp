87b8c4003739dbadd1c6d0d75f730e5c4ec529b9
nomliF <112778762+nomliF@users.noreply.github.com>
2023-11-01 14:45:27 +0100

    Array Och Std Vector
    Array Och Std Vector

---
+++ Labbar/Lab10/kap10CArrayOchStdVector.cpp
@@ -0,0 +1,10 @@
+#include <iostream> // innehaller cin , cout
+#include <cstdlib> // innehaller rand
+#include <cmath> // innehaller sqrt
+
+using namespace std ;
+
+
+void ingangTillCArrayOchStdVector () {
+    cout << endl << "IngangTillCArrayOchStdVector +++++ " << endl ;
+}
---
+++ Labbar/Lab10/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+void ingangTillCArrayOchStdVector ();
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    ingangTillCArrayOchStdVector();
+    cout << endl << endl;
+    return 0;
+}
