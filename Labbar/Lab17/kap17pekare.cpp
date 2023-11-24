13eb2abfdf6c003b3a949d3a731d28ddea16e35b
nomliF <gefi21gd@student.ju.se>
2023-11-24 13:47:53 +0100

    automattestaStrangfunktionerP

--- Labbar/Lab17/kap17pekare.cpp
+++ Labbar/Lab17/kap17pekare.cpp
@@ -14,8 +14,87 @@ void ovaPaPekarnotation () {
     assert ( x == 2.0) ;
     cout << "Oningen lyckades ." << endl ;
 }
+void bytVarden(int *pA, int *pB){
+    int temp = *pA;
+    *pA = *pB;
+    *pB = temp;
+
+}
+void bytVarden(double *pA, double *pB){
+    double temp = *pA;
+    *pA = *pB;
+    *pB = temp;
+}
+void automattestaBytVarden(){
+    cout << "testar bytvarden "<< endl;
+    int a=1;
+    int b= 2;
+    bytVarden(&a,&b);
+    assert(a==2 && b== 1);
+    double x = 1.5;
+    double y = 2.5;
+    bytVarden(&x,&y);
+    assert(x==2.5 && y==1.5);
+    cout << "Testet av bytVarden lyckades!" << endl;
+}
+int langd(const char *str){
+    const char *pCh = str;
+    while (*pCh != 0)
+        ++pCh;
+    return pCh -str;
+}
+void automattestaLangd(){
+    assert(langd("hej") == 3);
+    cout << "Testet av langd lyckades!" << endl;
+}
+bool arLikaP ( const char * str1 , const char * str2 ) {
+    for(;*str1!=0 && *str2!=0;++str1, ++str2)
+        if(*str1 != *str2)
+            return false;
+    return (*str1 == 0 && *str2==0);
+}
+void konverteraTillVersalerP ( char * str ){
+    for(; *str!=0; ++str)
+        *str = toupper(*str);
+}
+void konverteraTillRubrikP ( char * str ) {
+    char* prev = nullptr;
+    for (; *str != 0; ++str) {
+        if (prev == nullptr || *prev == ' ' || *prev == '\t') {
+            *str = toupper(*str);
+        } else {
+            *str = tolower(*str);
+        }
+        prev = str;
+    }
+}
+void automattestaStrangfunktionerP () {
+    cout << " automattestar strangfunktionerna -P" << endl ;
+    assert ( arLikaP (" abcd ", " abcd ") ) ;
+    assert ( ! arLikaP (" abcd ", " abcX ") );
+    assert ( ! arLikaP (" abcd ", " Xbcd ") );
+    assert ( !arLikaP ("abcd e", "abcd") );
+    assert ( !arLikaP ("abcd", "abcd e") );
+    char strA [] = " abc ABC 123 . ,- xyz ";
+    char strB [] = " ABC ABC 123 . ,- XYZ ";
+    konverteraTillVersalerP ( strA );
+    assert ( arLikaP ( strA , strB ) ) ;
+    char str1 [] = " this is a test ";
+    char str2 [] = " THIS IS A TEST ";
+    char rubrik [] = " This Is A Test ";
+    char tom [] = "";
+    konverteraTillRubrikP ( str1 );
+    konverteraTillRubrikP ( str2 );
+    konverteraTillRubrikP ( tom );
+    assert ( arLikaP ( str1 , rubrik ) );
+    assert ( arLikaP ( str2 , rubrik ) );
+    assert ( arLikaP ( tom , "") );
+    cout << " testen av strangfunktionerna -P lyckades !" << endl ;
+}
 
 void ingangTillPekare(){
     cout << "Ingang till pekare +++++++ "<< endl;
-    ovaPaPekarnotation();
+    automattestaStrangfunktionerP();
+    //automattestaLangd();
+    //ovaPaPekarnotation();
 }
